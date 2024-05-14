398f3f6a500a1f5e2928f6968b0f590adb65e102
reginox <99781653+reginoxcf@users.noreply.github.com>
2024-05-14 10:10:48 +0700

    Add files via upload

--- data-structures-algorithms/range_queries/segment_tree.cpp
+++ data-structures-algorithms/range_queries/segment_tree.cpp
@@ -1,37 +1,26 @@
 const ll maxn = 1e6+3;
 struct SegTree{
-	//Segment tree lazy propagation subarray sum
-	ll tree[maxn*4+1], lazy[maxn*4+1];
-	void reset(){
-		memset(tree,0,sizeof(tree));
-		memset(lazy,0,sizeof(lazy));
-	}
-	void down(ll id){
-		ll t = lazy[id];
-		tree[id*2+1]+=t;
-		tree[id*2]+=t;
-		lazy[id*2+1]+=t;
-		lazy[id*2]+=t;
-		lazy[id]=0;
-	}
-	void upd(ll id, ll l, ll r, ll u, ll v, ll val){
-		if(l>r||u>v) return ;
-		if(v<l||r<u) return ;
-		if(u<=l&&r<=v){
-			tree[id]+=val;
-			lazy[id]+=val;
-			return ;
-		}
-		down(id);
-		upd(id*2,l,(l+r)/2,u,v,val);
-		upd(id*2+1,(l+r+2)/2,r,u,v,val);
-		tree[id]=tree[id*2]+tree[id*2+1];
-	}
-	ll get(ll id, ll l, ll r, ll u, ll v){
-		if(u>v||l>r) return 0;
-		if(v<l||r<u) return 0;
-		if(u<=l&&r<=v) return tree[id];
-		down(id);
-		return get(id*2, l, (l+r)/2, u, v)+get(id*2+1, (l+r+2)/2, r, u, v);
-	}
+    //Segment tree subarray sum
+    ll tree[maxn*4+1];
+    void reset(){
+        memset(tree,0,sizeof(tree));
+    }
+    void upd(ll id, ll l, ll r, ll u, ll val){
+        if(r<u||u<l) return ;
+        if(l==r){
+            tree[id]=val;
+            return;
+        }
+        ll mid=(l+r)>>1;
+        upd(id*2,l,mid,u,val);
+        upd(id*2+1,mid+1,r,u,val);
+        tree[id]=tree[id*2]+tree[id*2+1];
+    }
+    ll get(ll id, ll l, ll r, ll u, ll v){
+        if(u>v||l>r) return 0;
+        if(v<l||r<u) return 0;
+        if(u<=l&&r<=v) return tree[id];
+        ll mid=(l+r)>>1;
+        return get(id*2, l, mid, u, v)+get(id*2+1, mid+1, r, u, v);
+    }
 } tree;
