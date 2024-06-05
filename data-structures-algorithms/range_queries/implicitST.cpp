2ec933a0d96b1950f9e8a542d11c821e2e04b5cf
reginox <99781653+reginoxcf@users.noreply.github.com>
2024-06-05 21:45:42 +0700

    new DSA: implict segtree (or dynamic)

---
+++ data-structures-algorithms/range_queries/implicitST.cpp
@@ -0,0 +1,31 @@
+const long long maxn = 5e5+3, inf = 1ll<<60;
+struct implicitST{
+    //Implicit(dynamic) segment tree - point add range sum
+    //Update: logn
+    //Query: logn
+    long long cl[maxn*4], cr[maxn*4], cnt=1;
+    long long st[maxn*4];
+    void update(long long id, long long l, long long r, long long u, long long val){
+        if(u < l || r < u) return ;
+        if(l == r){
+            st[id] += val;
+            return ;
+        }
+        long long mid = (l+r)>>1;
+        if(cl[id] == 0) cl[id] = ++cnt;
+        update(cl[id], l, mid, u, val);
+        if(cr[id] == 0) cr[id] = ++cnt;
+        update(cr[id], mid+1, r, u, val);
+        st[id] = st[cr[id]] + st[cl[id]];
+    }
+    long long query(long long id, long long l, long long r, long long u, long long v){
+        if(v < l || r < u || l > r || u > v) return 0;
+        if(u <= l && r <= v) return st[id];
+        long long vl, vr, mid = (l+r)>>1;
+        if(cl[id] == 0) vl = 0;
+        else vl = query(cl[id], l, mid, u, v);
+        if(cr[id] == 0) vr = 0;
+        else vr = query(cr[id], mid+1, r, u, v);
+        return vl + vr;
+    }
+} tree;
