711a4554b00d222f3708e765d59a62b65b3fa0b5
reginoxcf <1605giabao@gmail.com>
2025-01-05 15:41:21 +0700

    remove dsu.cpp extra code

--- DSA-templates/data-structures/dsu.cpp
+++ DSA-templates/data-structures/dsu.cpp
@@ -1,12 +1,3 @@
-//https://judge.yosupo.jp/problem/unionfind
-#include <bits/stdc++.h>
-#define ll long long
-#define ld long double
-#define all(v) begin(v), end(v)
-#define pi pair<int, int>
-#define vi vector<int>
-using namespace std;
-
 struct DSU{
 	int n;
 	vector<int> lab;
@@ -30,21 +21,3 @@ struct DSU{
 		lab[v] = u;
 	}
 };
-
-int main(){
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	int n, t; cin >> n >> t;
-	DSU dsu(n);
-	while(t--){
-		int x, u, v; cin >> x;
-		if(x==0){
-			cin >> u >> v;
-			dsu.join(u+1, v+1);
-		}
-		else{
-			cin >> u >> v;
-			cout << (dsu.find(u+1) == dsu.find(v+1)? "1\n":"0\n");
-		}
-	}
-	return 0;
-}
